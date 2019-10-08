daaa72a9137d073a218c04e170b42cf670f77367
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-08 22:10:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,7 +15,7 @@ require 'pry'
         :description =>
             project.css("p.bbcard_blurb").text,
         :location => project.css("ul.project-meta span.location-name").text,
-        :percent_funded => project.ccs("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+        :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
       }
   end
 
